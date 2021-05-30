2e5c8c873392f3752d257db92504631dd8252f8c
Ivan Sanchez <edgarse1945@gmail.com>
2021-05-29 19:31:56 -0500

    Training algorithm implemented

--- mlp.py
+++ mlp.py
@@ -3,7 +3,9 @@ from ActivationFunctions import * # import activation functions
 
 ### MLP class definition
 class MLP:
-    def __init__(self, layers_dim, output_activation=logistic, hidden_activation=tanh):
+    def __init__(self, layers_dim, 
+                 output_activation=logistic, 
+                 hidden_activation=tanh):
         
         #### Atributes declaration ####
         # Number of layers in the network
@@ -22,7 +24,7 @@ class MLP:
             
             if l == self.L:  # if is the last layer
                 self.f[l] = output_activation
-            else:           # is is a hidden layer
+            else:           #if is a hidden layer
                 self.f[l] = hidden_activation
         
     def predict(self, X):
@@ -33,5 +35,36 @@ class MLP:
             a = self.f[l](z)
         return a
         
+    def fit(self, X, Y, epochs=500, lr=0.1):
+        
+        """
+            Training algorithm used: Stochastic Gradient Descent (SGD)
+        """
+        P = X.shape[1]          # Number of patterns 
+        for _ in range(epochs): # for every epoch
+            for p in range(P):  # for every pattern
+                
+                ### Initialize
+                lg = [None] * (self.L + 1)          # local gradient (lg)
+                a = [None] * (self.L + 1)           # output of every layer (a)
+                da = [None] * (self.L + 1)          # derivative of a (da)
+                
+                ### Propagation
+                a[0] = X[:, p].reshape(-1, 1)       # first entry 
+                for l in range(1, self.L + 1):      # for every layer
+                    z = np.dot(self.w[l], a[l-1]) + self.b[l]
+                    a[l], da[l] = self.f[l](z, derivative=True)
+                         
+                ### Backpropagation
+                for l in range(self.L, 0, -1):
+                    if l == self.L:
+                        lg[l] = (Y[:, p].reshape(-1, 1) - a[l]) * da[l]
+                    else:
+                        lg[l] = np.dot(self.w[l+1].T, lg[l+1]) * da[l]
+                
+                ### Gradient descent
+                for l in range(1, self.L + 1):
+                    self.w += lr * np.dot(lg[l], a[l-1].T)
+                    self.b += lr * lg[l]                    
         
         
