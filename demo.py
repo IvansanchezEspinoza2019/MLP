000ebc550d73be61c967cbcec03c152b57d780b0
Ivan Sanchez <edgarse1945@gmail.com>
2021-05-30 21:57:04 -0500

    Circles problem implemented

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ import pandas as pd
 from mlp import *
 
 
-def draw2DNetData(x, y, model, title): 
+def draw2DNetData(x, y, model, title, pointSize=5): 
     """
         Plot the decision surface for 2-class problems
     """
@@ -18,9 +18,9 @@ def draw2DNetData(x, y, model, title):
     ## plot every single pattern of every class in the graph 
     for i in range(x.shape[1]):
         if y[0, i] == 1:
-            plt.plot(x[0, i], x[1, i], '.b', markersize=15)   # class 0
+            plt.plot(x[0, i], x[1, i], '.b', markersize=pointSize)   # class 0
         else:
-            plt.plot(x[0, i], x[1, i], '.r', markersize=15)   # class 1
+            plt.plot(x[0, i], x[1, i], '.r', markersize=pointSize)   # class 1
             
     ## get the limits of every dimention (1 and 2 only)
     xmin, ymin = np.min(x[0, :])-0.5, np.min(x[1, :])-0.5   # minimum value of dimension 1 and 2 (X[0], X[1])
@@ -51,7 +51,7 @@ def draw2DNetData(x, y, model, title):
 
 
 
-problem = 1
+problem = 2
 
 if problem == 1: # XOR
     
@@ -75,3 +75,32 @@ if problem == 1: # XOR
     # draw graph after training
     draw2DNetData(X, Y, net,"XOR After Training")
         
+elif problem == 2: # CIRCLES
+    
+    # read csv
+    data = pd.read_csv("datasets/circles.csv")
+    
+    # entries
+    X = np.asanyarray(data.drop(columns=["y"])).T
+    # output class
+    Y = np.asanyarray(data[['y']]).T
+    
+    # create our network
+    net = MLP((2,5,5,1), output_activation=logistic, hidden_activation=tanh)
+    
+    # before training
+    draw2DNetData(X, Y, net, "Model before training",pointSize=7)
+    
+    # train the network
+    net.fit(X, Y, epochs=250, lr=0.1)
+    
+    # after training
+    draw2DNetData(X, Y, net, "Model after training", pointSize=7)
+    
+    
+    
+    
+    
+    
+    
+    
